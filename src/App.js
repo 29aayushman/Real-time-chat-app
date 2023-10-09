19cfe828a42b7c0d5cd3d9558e91e91749d04559
29aayushman <29aayushman@gmail.com>
2023-10-09 19:40:49 +0530

    firebase.js added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Login/>
     </div>
   );
 }
---
+++ src/pages/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getauth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlnBSBBegFwxQ9FvmJhEoB0dwGrfi_4T4",
+  authDomain: "realtimechat-75db4.firebaseapp.com",
+  projectId: "realtimechat-75db4",
+  storageBucket: "realtimechat-75db4.appspot.com",
+  messagingSenderId: "750710689877",
+  appId: "1:750710689877:web:b11a235d46e0b2ad2084dc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth= getauth()
