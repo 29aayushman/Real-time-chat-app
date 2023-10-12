c2c41c62e162da14d062fc1ea06303f57c12e82e
Jiya Thakur <142887456+jiyaaat@users.noreply.github.com>
2023-10-12 17:51:38 +0530

    Update Navbar.js

--- src/Chatpage/Navbar.js
+++ src/Chatpage/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-      <span className="logo">Whatsup</span>
+      <span className="logo">Chatapp</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
